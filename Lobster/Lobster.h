194da642141fbd66a16c0afe62ae9a777417c407
sgr-ksmt <melodydance.k.s@gmail.com>
2017-10-31 23:27:48 +0900

    initial setup.

---
+++ Lobster/Lobster.h
@@ -0,0 +1,19 @@
+//
+//  Lobster.h
+//  Lobster
+//
+//  Created by suguru-kishimoto on 2017/10/31.
+//  Copyright © 2017年 Suguru Kishimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lobster.
+FOUNDATION_EXPORT double LobsterVersionNumber;
+
+//! Project version string for Lobster.
+FOUNDATION_EXPORT const unsigned char LobsterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lobster/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'Lobster' do
+  pod 'Firebase/RemoteConfig'
+end
