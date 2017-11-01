12778b08ab47d64280badbef32e0faffaaf5679b
sgr-ksmt <melodydance.k.s@gmail.com>
2017-11-02 03:26:13 +0900

    setup demo.

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by suguru-kishimoto on 2017/11/02.
+//  Copyright © 2017年 Suguru Kishimoto. All rights reserved.
+//
+
+import UIKit
+import Lobster
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
